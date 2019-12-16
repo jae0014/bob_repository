d36b34eb34b558ffed0f7ee9a0ef29c1d383653b
jae0014 <jaedafeeder@gmail.com>
2019-12-16 14:14:43 +0900

    sad

--- BobJo/src/reply/model/dao/ReplyDao.java
+++ BobJo/src/reply/model/dao/ReplyDao.java
@@ -2,9 +2,7 @@ package reply.model.dao;
 
 import static common.JDBCTemplate.close;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,18 +18,14 @@ public class ReplyDao {
 	private Properties prop =null;
 	
 	public ArrayList<Reply> selectAll(Connection conn, String nPost , int type) {
+		prop = new Properties();
 		ArrayList<Reply> list = new ArrayList<Reply>();
 		PreparedStatement pstmt = null;
 		ResultSet rset = null;
-		String fileName = ReplyDao.class.getResource("/sql/reply/reply.properties").getPath();
-		
+		System.out.println("you are here");
 		try {
-			System.out.println(fileName);
-			prop.load(new FileReader(fileName));
-			
-			String sql = prop.getProperty("selectAll");
-			System.out.println("sql");
-			System.out.println(sql);
+			String sql ="";
+			sql = "SELECT * FROM reply where BR_ID = ? AND BTYPE = ?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, nPost);
 			pstmt.setInt(2, type);
@@ -44,26 +38,19 @@ public class ReplyDao {
 				String content = rset.getString("c_content");
 				Date date = rset.getDate("c_date");
 				String status = rset.getString("c_status");
-				String m_name = rset.getString("m_name");
-				
+				String m_name = rset.getString("m_name");	
 				Reply comment = new Reply(cid,btye,brid,mno,content,date,status,m_name);
 				list.add(comment);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally
+		}  finally
 		{	close(rset);
 			close(pstmt);
 		}
 		
-
+		System.out.println("returning: " + list.size());
 		return list;
 	}
 }
