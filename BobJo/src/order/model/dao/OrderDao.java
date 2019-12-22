331cc986e16742d3f43bcb98a0259d93da24fb81
wkdgp <wkdgp@DESKTOP-Q5005OB>
2019-12-22 22:03:07 +0900

    191222 혜림 - 주문 및 결제 후 데이터 처리

--- BobJo/src/order/model/dao/OrderDao.java
+++ BobJo/src/order/model/dao/OrderDao.java
@@ -56,6 +56,89 @@ public class OrderDao {
 		
 		return list;
 	}
+
+	// 주문 추가하기
+	public int insertOrder(Connection conn, String mNo, String rName, String rPhone, String addr, String rPlease,
+			int chong) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("insertOrder");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, mNo);
+			pstmt.setString(2, rName);
+			pstmt.setString(3, rPhone);
+			pstmt.setString(4, addr);
+			pstmt.setString(5, rPlease);
+			pstmt.setInt(6, chong);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		return result;
+	}
+
+	// 주문 상세 추가하기
+	public int insertDetailOrder(Connection conn, String orderId, String pId, int quantity, int total) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		
+		String sql = prop.getProperty("inserDetailOrder");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, orderId);
+			pstmt.setString(2, pId);
+			pstmt.setInt(3, quantity);
+			pstmt.setInt(4, total);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	// 최근 주문번호 1개 조회하기
+	public String selectOrderId(Connection conn, String mNo) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String orderId = "";
+		
+		String sql = prop.getProperty("selectOrderId");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, mNo);
+			
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				orderId = rset.getString(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return orderId;
+	}
 	
 	
 	
--- BobJo/src/order/model/service/OrderService.java
+++ BobJo/src/order/model/service/OrderService.java
@@ -22,4 +22,30 @@ public class OrderService {
 		
 	}
 
+	// Shopping_Order 테이블 접근 -> 주문 추가하기
+	public int insertOrder(String mNo, String rName, String rPhone, String addr, String rPlease, int chong) {
+		Connection conn = getConnection();
+		int r1 = new OrderDao().insertOrder(conn, mNo, rName, rPhone, addr, rPlease, chong);
+		
+		close(conn);
+		return r1;
+	}
+
+	// order_detail 테이블 접근 -> 주문 상세 추가하기
+	public int insertDetailOrder(String orderId, String pId, int quantity, int total) {
+		Connection conn = getConnection();
+		int result = new OrderDao().insertDetailOrder(conn, orderId, pId, quantity, total);
+		
+		close(conn);
+		return result;
+	}
+
+	// 최근 주문 번호 1개 조회하기
+	public String selectOrderId(String mNo) {
+		Connection conn = getConnection();
+		String orderId = new OrderDao().selectOrderId(conn, mNo);
+		close(conn);
+		return orderId;
+	}
+
 }
