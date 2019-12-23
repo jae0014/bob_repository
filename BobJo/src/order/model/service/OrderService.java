8cea44b001fb5e692de555852965bf4723c11bf7
wkdgp <wkdgp@DESKTOP-Q5005OB>
2019-12-23 23:29:53 +0900

    191223 혜림 - 주문, 상품페이징처리

--- BobJo/src/order/model/service/OrderService.java
+++ BobJo/src/order/model/service/OrderService.java
@@ -1,6 +1,6 @@
 package order.model.service;
 
-import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.*;
 import static common.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
@@ -10,6 +10,7 @@ import notice.model.dao.NoticeDao;
 import notice.model.vo.Notice;
 import order.model.dao.OrderDao;
 import order.model.vo.Order;
+import order.model.vo.OrderDetail;
 
 public class OrderService {
 
@@ -26,7 +27,11 @@ public class OrderService {
 	public int insertOrder(String mNo, String rName, String rPhone, String addr, String rPlease, int chong) {
 		Connection conn = getConnection();
 		int r1 = new OrderDao().insertOrder(conn, mNo, rName, rPhone, addr, rPlease, chong);
-		
+		if(r1>0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
 		close(conn);
 		return r1;
 	}
@@ -35,7 +40,11 @@ public class OrderService {
 	public int insertDetailOrder(String orderId, String pId, int quantity, int total) {
 		Connection conn = getConnection();
 		int result = new OrderDao().insertDetailOrder(conn, orderId, pId, quantity, total);
-		
+		if(result>0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
 		close(conn);
 		return result;
 	}
@@ -46,6 +55,14 @@ public class OrderService {
 		String orderId = new OrderDao().selectOrderId(conn, mNo);
 		close(conn);
 		return orderId;
+	}
+
+	// 마이페이지에서 주문 조회 시
+	public ArrayList<OrderDetail> selectMyOrder(String mNo) {
+		Connection conn = getConnection();
+		ArrayList<OrderDetail> oList = new OrderDao().selectMyOrder(conn, mNo);
+		close(conn);
+		return oList;
 	}
 
 }
